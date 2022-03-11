8356ab6e23322d811040dbe997d1ee7858c8e606
codenamenam <bluetire27@gmail.com>
2022-03-11 15:51:54 +0900

    Remove 'Management' project

--- Management/simple/urls.py
+++
@@ -1,9 +0,0 @@
-from unicodedata import name
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("sub", views.sub, name="sub")
-]
--- Management/simple/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    return render(request, "index.html")
-
-def sub(request):
-    return render(request, "sub.html")
